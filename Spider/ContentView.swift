5a0e94b53c1a04b4562ab0a745798b187915be8e
superkookai <weerawut@live.com>
2025-05-01 09:32:18 +0700

    Initial Commit

---
+++ Spider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spider
+//
+//  Created by Weerawut Chaiyasomboon on 01/05/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spider/SpiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiderApp.swift
+//  Spider
+//
+//  Created by Weerawut Chaiyasomboon on 01/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct SpiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
